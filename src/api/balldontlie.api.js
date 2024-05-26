e16efdb793a167e929336b409a36d5730665286b
Ionfinisher <teddyassih@gmail.com>
2024-05-26 20:41:04 +0000

    add function to get teams with axios

--- src/api/balldontlie.api.js
+++ src/api/balldontlie.api.js
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const retrievePosts = async () => {
+const retrieveTeams = async () => {
   const response = await axios({
     method: "GET",
     url: "https://api.balldontlie.io/v1/teams",
@@ -10,21 +10,4 @@ const retrievePosts = async () => {
     },
   });
   return response.data;
-};
-
-export const fetchGithubUser = async () => {
-  const res = await fetch("https://api.github.com/users/kiranm27");
-  if (!res.ok) {
-    throw new Error("Network response was not ok");
-  }
-  const data = await res.json();
-  const user: TGithubUser = {
-    name: data?.name || "",
-    bio: data?.bio || "",
-  };
-  return user;
-};
-
-export const useGithubUser = () => {
-  return useQuery(["githubUser"], fetchGithubUser);
 };
